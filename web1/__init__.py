9bb2091ac81fcfc9b55aab1b8b64b204e316fc9f
Abhishek974823 <abhishekjha17.11.2004@gmail.com>
2026-09-18 01:27:33 +0530

    postrage added

--- web1/__init__.py
+++ web1/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 db = SQLAlchemy()
 def create_app1():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hanadjnfdka'
     from .first import views
     app.register_blueprint(views,url_prefix='/')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
     db.init_app(app)
     return app
